85862be04512b19ab0f90ffc692b3e35088e1054
vanlinh1330 <levanlinh1330@gamil.com>
2022-12-14 02:01:38 +0700

    Cấu hình Router/Layout cho dự án

--- src/App.js
+++ src/App.js
@@ -9,8 +9,18 @@ function App() {
             <div className="App">
                 <Routes>
                     {publicRoutes.map((route, index) => {
-                        const Layout = route.layout === null ? Fragment : DefaultLayout;
                         const Page = route.component;
+
+                        // const Layout = route.layout === null ? Fragment : DefaultLayout;
+
+                        let Layout = DefaultLayout;
+
+                        if (route.layout) {
+                            Layout = route.layout;
+                        } else if (route.layout === null) {
+                            Layout = Fragment;
+                        }
+
                         return (
                             <Route
                                 key={index}
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,20 @@
+// Layouts
+import { HeaderOnly } from '~/components/Layout';
+
+// Pages
 import Home from '~/pages/Home';
 import Following from '~/pages/Following';
 import Profile from '~/pages/Profile';
 import Upload from '~/pages/Upload';
+import Search from '~/pages/Search';
 
 // Public routes : ko can dang nhap vao dc
 const publicRoutes = [
     { path: '/', component: Home },
     { path: '/following', component: Following },
     { path: '/profile', component: Profile },
-    { path: '/upload', component: Upload, layout: null },
+    { path: '/upload', component: Upload, layout: HeaderOnly },
+    { path: '/search', component: Search, layout: null },
 ];
 
 // Private routes : dang nhap vao dc
